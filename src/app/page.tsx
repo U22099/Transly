3f19f7df254fc71474e0639b45922c6b29e56f1f
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-01 21:11:32 +0000

    ---

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-"use client"
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main
-     className="flex flex-col justify-center items-center backdrop-blur-md gap-4 text-center bg-slate-100 dark:bg-violet-950 h-screen">
-      <h1 className="text-5xl md:text-7xl font-monospace dark:invert text-bold">Welcome to Transly</h1>
-      <p className="font-monospace dark:invert"> A language translator with support for over 50+ languages and voice feature </p>
-      <Link href="/home" className="animate-bounce bg-none border-black border-2 p-5 px-16 rounded-full text-bold font-monospace dark:invert dark:bg-white dark:border-none">Enter</Link>
+    <main className="flex flex-col justify-center items-center gap-4 text-center bg-slate-100 dark:bg-gray-900 h-screen">
+      <h1 className="text-5xl md:text-7xl font-mono dark:text-white text-bold">
+        Welcome to Transly
+      </h1>
+      <p className="font-mono dark:text-white max-w-3/4">
+        A language translator with support for over 50+ languages and voice
+        feature
+      </p>
+      <Link
+        href="/home"
+        className="animate-bounce bg-none border-black border-2 p-5 px-16 rounded-full text-bold font-mono dark:text-black dark:bg-white dark:border-none mt-5 text-2xl md:text-4xl hover:transform hover:animate-none hover:scale-[1.02] transition-transform duration-1000"
+      >
+        Enter
+      </Link>
     </main>
   );
 }
