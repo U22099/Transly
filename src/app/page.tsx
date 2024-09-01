7ed196ee93455190fb4abf3b96ba3ec82ad0eb87
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-01 20:04:49 +0000

    —-

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main
-     className="flex flex-col justify-center items-center backdrop-blur-md gap-4 text-center bg-slate-100 dark:bg-violet-950">
+     className="flex flex-col justify-center items-center backdrop-blur-md gap-4 text-center bg-slate-100 dark:bg-violet-950 h-screen">
       <h1 className="text-5xl md:text-7xl font-monospace dark:invert text-bold">Welcome to Transly</h1>
       <p className="font-monospace dark:invert"> A language translator with support for over 50+ languages and voice feature </p>
-      <Link href="/home" className="animate-bounce bg-none border-black border-2 p-5 rounded-full text-bold font-monospace dark:invert dark:bg-white dark:border-none">Enter</Link>
+      <Link href="/home" className="animate-bounce bg-none border-black border-2 p-5 px-16 rounded-full text-bold font-monospace dark:invert dark:bg-white dark:border-none">Enter</Link>
     </main>
   );
 }
