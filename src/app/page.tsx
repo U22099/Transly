26404d0959a962490233a596a4a9963f73f04c4e
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-01 19:18:05 +0100

    ---

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import {motion} from 'framer-motion';
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center backdrop-blur-md gap-4 text-center">
+    <motion.main
+    initial={{opacity: 0, y: -100}}
+    animate={{opacity: 1, y: 0}}
+     className="flex flex-col justify-center items-center backdrop-blur-md gap-4 text-center">
       <h1 className="text-5xl md:text-7xl font-monospace dark:invert text-bold">Welcome to Transly</h1>
       <p className="font-monospace dark:invert"> A language translator with support for over 50+ languages and voice feature <p>
       <button className="bg-none border-black border-2 p-5 rounded-full text-bold font-monospace dark:invert dark:bg-white dark:border-none">Enter</button>
-    </main>
+    </motion.main>
   );
 }
